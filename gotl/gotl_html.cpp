23656de09d421db54286d7b53c15db7ef5b47e52
asm128 <pabloarielz@outlook.com>
2019-07-06 23:01:06 -0300

    + Added \n.

--- gotl/gotl_html.cpp
+++ gotl/gotl_html.cpp
@@ -17,17 +17,17 @@ const char *	scripts []	=
 
 ::gotl::error_t			gotl::generateHTMLOutput	(::std::string & output		)	{ 
 	output					= "<html>" ; 
-	output					+= "<head><title>CGI World</title></head>" ; 
+	output					+= "\n<head><title>CGI World</title></head>" ; 
 
-	output					+= "<body>" ; 
-	output					+= "<table><tr>" ; 
+	output					+= "\n<body>" ; 
+	output					+= "\n<table><tr>" ; 
 	for(uint32_t iOption = 0; iOption < ::std::size(options); ++iOption) 
-		output					+= "<td>" + ::std::string(options[iOption])+ "</td>" ; 
-	output					+= "</tr></table>" ; 
-	output					+= "</body>" ; 
+		output					+= "\n<td>" + ::std::string(options[iOption])+ "</td>" ; 
+	output					+= "\n</tr></table>" ; 
+	output					+= "\n</body>" ; 
 
 	for(uint32_t iScript = 0; iScript < ::std::size(scripts); ++iScript) 
-		output					+= "<script>" + ::std::string(scripts[iScript])+ "</script>" ; 
-	output					+= "</html>" ; 
+		output					+= "\n<script>" + ::std::string(scripts[iScript])+ "</script>" ; 
+	output					+= "\n</html>" ; 
 	return 0; 
 }
